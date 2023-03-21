1a4344a78907c639a86ccf695328225da53e190d
Thaweewat <104571571+RThaweewat@users.noreply.github.com>
2023-03-21 22:58:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-def process_file(file):
+def process_file(file, df):
     if file is None:
         return None, None
 
@@ -13,13 +13,15 @@ def process_file(file):
     if "status" not in df.columns:
         df["status"] = ""
 
-    return df, df[df["status"] == ""]
+    df_unfilled = df[df["status"] == ""]
+
+    return df, df_unfilled
 
 def main():
     st.title("Address Annotation App")
 
     uploaded_file = st.file_uploader("Upload a CSV or Excel file", type=["csv", "xlsx"])
-    df, df_unfilled = process_file(uploaded_file)
+    df, df_unfilled = process_file(uploaded_file, None)
 
     if df is not None:
         if "index" not in st.session_state:
@@ -47,6 +49,9 @@ def main():
             if st.button("Back") and index > 0:
                 index -= 1
                 st.session_state.index = index
+
+            # Update df_unfilled after changing the status
+            _, df_unfilled = process_file(uploaded_file, df)
 
             unfilled_count = len(df) - filled_count - (index + 1)
             st.write(f"{filled_count} rows filled, {unfilled_count} rows remaining")
